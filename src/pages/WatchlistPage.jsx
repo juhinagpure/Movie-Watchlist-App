7a3e1ca1da7b09459ace884b616e55bfdbeabe49
JUHI NAGPURE <juhinagpure143@gmail.com>
2024-11-06 23:45:19 +0530

    refactor: Update styling in index.html and WatchlistPage.jsx

--- src/pages/WatchlistPage.jsx
+++ src/pages/WatchlistPage.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import { useParams, useNavigate } from 'react-router-dom';
+import { useSelector } from "react-redux";
+import { useParams, useNavigate } from "react-router-dom";
 
 const WatchlistPage = () => {
   const { listName } = useParams();
-  const user = useSelector((state) => state.user.users.find((u) => u.email === state.user.currentUser));
+  const user = useSelector((state) =>
+    state.user.users.find((u) => u.email === state.user.currentUser)
+  );
   const watchlistMovies = user?.watchlists[listName] || [];
   const navigate = useNavigate();
 
@@ -14,8 +15,16 @@ const WatchlistPage = () => {
       {watchlistMovies.length > 0 ? (
         <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4">
           {watchlistMovies.map((movie) => (
-            <div key={movie.imdbID} className="cursor-pointer bg-gray-100 rounded p-2 shadow-md flex flex-col items-center" onClick={() => navigate(`/movie/${movie.imdbID}`)}>
-              <img src={movie.Poster} alt={movie.Title} className="mb-2 aspect-[3/4]" />
+            <div
+              key={movie.imdbID}
+              className="cursor-pointer bg-gray-100 rounded p-2 shadow-md flex flex-col items-center"
+              onClick={() => navigate(`/movie/${movie.imdbID}`)}
+            >
+              <img
+                src={movie.Poster}
+                alt={movie.Title}
+                className="mb-2 aspect-[3/4]"
+              />
               <p className="text-sm font-semibold text-center">{movie.Title}</p>
               <p className="text-xs text-gray-500">{movie.Year}</p>
             </div>
